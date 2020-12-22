713bf3423f2727415be5f2f6fd89a519bf25b526
Tristan <huohangyu0214@163.com>
2020-12-22 16:42:02 +0800

    添加了私信列表的部分功能

---
+++ src/main/java/com/nowcoder/community/service/MessageService.java
@@ -0,0 +1,66 @@
+package com.nowcoder.community.service;
+
+import com.nowcoder.community.dao.MessageMapper;
+import com.nowcoder.community.entity.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MessageService {
+
+    @Autowired
+    private MessageMapper messageMapper;
+
+    /**
+     * 查询会话业务
+     * @param userId
+     * @param offset
+     * @param limit
+     * @return
+     */
+    public List<Message> findConversation(int userId, int offset, int limit) {
+        return messageMapper.selectConversations(userId, offset, limit);
+    }
+
+    /**
+     * 查询会话数量业务
+     * @param userId
+     * @return
+     */
+    public int findConversationCount(int userId) {
+        return messageMapper.selectConversationCount(userId);
+    }
+
+    /**
+     * 查询私信列表业务
+     * @param conversationId
+     * @param offset
+     * @param limit
+     * @return
+     */
+    public List<Message> findLetters(String conversationId, int offset, int limit) {
+        return messageMapper.selectLetters(conversationId, offset, limit);
+    }
+
+    /**
+     * 查询私信数量业务
+     * @param conversationId
+     * @return
+     */
+    public int findLetterCount(String conversationId) {
+        return messageMapper.selectLetterCount(conversationId);
+    }
+
+    /**
+     * 查询未读私信数量
+     * @param userId
+     * @param conversationId
+     * @return
+     */
+    public int findLetterUnreadCount(int userId, String conversationId) {
+        return messageMapper.selectLetterUnreadCount(userId, conversationId);
+    }
+
+}
