03d8889d8a730b01090be8c9fae81129921ff1d0
Tristan <huohangyu0214@163.com>
2020-12-25 15:29:28 +0800

    添加了点赞功能

---
+++ src/main/java/com/nowcoder/community/service/LikeService.java
@@ -0,0 +1,53 @@
+package com.nowcoder.community.service;
+
+import com.nowcoder.community.util.RedisKeyUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LikeService {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    /**
+     * 点赞业务
+     * @param userId
+     * @param entityType
+     * @param entityId
+     */
+    public void like(int userId, int entityType, int entityId) {
+        String entityLikeKey = RedisKeyUtil.getEntityLikeKey(entityType, entityId);
+        boolean isMember = redisTemplate.opsForSet().isMember(entityLikeKey, userId);
+        if(isMember) {
+            redisTemplate.opsForSet().remove(entityLikeKey, userId);
+        } else {
+            redisTemplate.opsForSet().add(entityLikeKey, userId);
+        }
+    }
+
+    /**
+     * 查询某实体点赞的数量业务
+     * @param entityType
+     * @param entityId
+     * @return
+     */
+    public long findEntityLikeCount(int entityType, int entityId) {
+        String entityLikeKey = RedisKeyUtil.getEntityLikeKey(entityType, entityId);
+        return redisTemplate.opsForSet().size(entityLikeKey);
+    }
+
+    /**
+     * 查询某人对某实体的点赞状态业务
+     * @param userId
+     * @param entityType
+     * @param entityId
+     * @return 返回1表示用户点赞，返回0表示用户未点赞
+     */
+    public int findEntityLikeStatus(int userId, int entityType, int entityId) {
+        String entityLikeKey = RedisKeyUtil.getEntityLikeKey(entityType, entityId);
+        return redisTemplate.opsForSet().isMember(entityLikeKey, userId) ? 1 : 0;
+    }
+
+}
