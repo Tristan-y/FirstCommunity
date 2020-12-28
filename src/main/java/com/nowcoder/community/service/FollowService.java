9fd24bdba0c989d9f743a44544a4f944e9760cae
Tristan <huohangyu0214@163.com>
2020-12-28 11:07:56 +0800

    添加关注、取消关注功能

---
+++ src/main/java/com/nowcoder/community/service/FollowService.java
@@ -0,0 +1,93 @@
+package com.nowcoder.community.service;
+
+import com.nowcoder.community.util.RedisKeyUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SessionCallback;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FollowService {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    /**
+     * 关注业务
+     * @param userId
+     * @param entityType
+     * @param entityId
+     */
+    public void follow(int userId, int entityType, int entityId) {
+        redisTemplate.execute(new SessionCallback() {
+            @Override
+            public Object execute(RedisOperations operations) throws DataAccessException {
+                String followeeKey = RedisKeyUtil.getFolloweeKey(userId, entityType);
+                String followerKey = RedisKeyUtil.getFollowerKey(entityType, entityId);
+
+                operations.multi();
+                operations.opsForZSet().add(followeeKey, entityId, System.currentTimeMillis());
+                operations.opsForZSet().add(followerKey, userId, System.currentTimeMillis());
+                return operations.exec();
+            }
+        });
+    }
+
+    /**
+     * 取关业务
+     * @param userId
+     * @param entityType
+     * @param entityId
+     */
+    public void unfollow(int userId, int entityType, int entityId) {
+        redisTemplate.execute(new SessionCallback() {
+            @Override
+            public Object execute(RedisOperations operations) throws DataAccessException {
+                String followeeKey = RedisKeyUtil.getFolloweeKey(userId, entityType);
+                String followerKey = RedisKeyUtil.getFollowerKey(entityType, entityId);
+
+                operations.multi();
+                operations.opsForZSet().remove(followeeKey, entityId);
+                operations.opsForZSet().remove(followerKey, userId);
+                return operations.exec();
+            }
+        });
+    }
+
+    /**
+     * 统计用户关注的实体数量业务
+     * @param userId
+     * @param entityType
+     * @return
+     */
+    public long findFolloweeCount(int userId, int entityType) {
+        String followeeKey = RedisKeyUtil.getFolloweeKey(userId, entityType);
+        return redisTemplate.opsForZSet().zCard(followeeKey);
+    }
+
+    /**
+     * 统计实体的粉丝数量业务
+     * @param entityType
+     * @param entityId
+     * @return
+     */
+    public long findFollowerCount(int entityType, int entityId) {
+        String followerKey = RedisKeyUtil.getFollowerKey(entityType, entityId);
+        return redisTemplate.opsForZSet().zCard(followerKey);
+    }
+
+    /**
+     * 查询当前用户是否关注该实体
+     * @param userId
+     * @param entityType
+     * @param entityId
+     * @return
+     */
+    public boolean hasFollowed(int userId, int entityType, int entityId) {
+        String followeeKey = RedisKeyUtil.getFolloweeKey(userId, entityType);
+        return redisTemplate.opsForZSet().score(followeeKey, entityId) != null;
+    }
+
+}
